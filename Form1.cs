88f790376976d96b6b8e48d9d381dcfd933d5dec
jrf-g <jrfaragalla@gmail.com>
2026-07-06 13:05:55 -0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
+namespace CounterWinForms
+{
+    public partial class Form1 : Form
+    {
+        private uint counter = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            counter++;
+            num.Text = counter.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (counter > 0)
+            {
+                counter--;
+                num.Text = counter.ToString();
+            } else {
+                MessageBox.Show("The counter is unable to compute signed integers", "Alert", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            counter = 0;
+            num.Text = counter.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void num_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
